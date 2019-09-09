b7ae9c9b611eb2c08f14411645ccfd3f0829b8ae
ayakhont <Iohanga731!>
2019-09-09 14:54:16 +0200

    project for LB-2 0.03

--- LB-2/project/DsspStatistic.py
+++ LB-2/project/DsspStatistic.py
@@ -16,13 +16,16 @@ class SecondaryStructure:
         self.h_count = 0
         self.c_count = 0
 
-    def update(self, ss):
-        if ss == "E":
+    def update_counter(self, letter_for_secondary_structure):
+        if letter_for_secondary_structure == "E":
             self.e_count += 1
-        if ss == "H":
+        if letter_for_secondary_structure == "H":
             self.h_count += 1
-        if ss == "-":
+        if letter_for_secondary_structure == "-":
             self.c_count += 1
+
+    def get_string(self):
+        return "Strand: {}, Helix: {}, Coil: {}".format(self.e_count, self.h_count, self.c_count)
 
     def plot_pie_chart(self):
         labels = 'Strand', 'Helix', 'Coil'
@@ -32,6 +35,51 @@ class SecondaryStructure:
         ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
         ax1.axis('equal')
         plt.show()
+
+
+def calculate_statistics(ss_composition: SecondaryStructure, residue_composition: dict):
+    try:
+        for dssp_filename in os.listdir(dssp_dir):
+            with open(dssp_dir + dssp_filename, "r") as dssp_file:
+                fasta_filename = dssp_filename.replace("dssp", "fasta")
+                with open(fasta_dir + fasta_filename, "r") as fasta_file:
+                    fasta_line = ''
+                    for line in fasta_file:
+                        if line[0] != '>':
+                            fasta_line = line
+                    for dssp_line in dssp_file:
+                        if dssp_line[0] != '>':
+                            ss_composition.e_count += dssp_line.count("E")
+                            ss_composition.h_count += dssp_line.count("H")
+                            ss_composition.c_count += dssp_line.count("-")
+
+                            if dssp_line.__len__() == fasta_line.__len__():
+                                for i in range(0, dssp_line.__len__()):
+                                    residue = fasta_line[i]
+                                    ss = dssp_line[i]
+                                    if residue in residue_composition.keys():
+                                        obj = residue_composition.get(residue)
+                                    else:
+                                        obj = SecondaryStructure()
+                                    obj.update_counter(ss)
+                                    residue_composition[residue] = obj
+                            else:
+                                print("These files contains different length of string in sequences: ",
+                                      dssp_file)
+    except IOError:
+        print("IO error!!")
+
+
+def print_statistics(ss_composition: SecondaryStructure, residue_composition: dict):
+    common_character = ss_composition.e_count + ss_composition.h_count + ss_composition.c_count
+    print("Strand count: ", ss_composition.e_count)
+    print("Helix count: ", ss_composition.h_count)
+    print("Coil count: ", ss_composition.c_count)
+    print("Common character: ", common_character)
+
+    print("Residue composition: ")
+    for key in residue_composition.keys():
+        print("{}: {}".format(key, residue_composition.get(key).get_string()))
 
 
 def plot_bar_chart(residue_composition: dict):
@@ -51,10 +99,10 @@ def plot_bar_chart(residue_composition: dict):
     width = 0.35  # the width of the bars
 
     fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width / 2, e_means, width, label='Strand')
-    rects2 = ax.bar(x - width / 4, h_means, width, label='Helix')
-    rects3 = ax.bar(x + width / 4, c_means, width, label='Coil')
-    rects4 = ax.bar(x + width / 2, total_means, width, label='Total')
+    rects1 = ax.bar(x - width / 2, e_means, width/4, label='Strand')
+    rects2 = ax.bar(x - width / 4, h_means, width/4, label='Helix')
+    rects3 = ax.bar(x + width / 4, c_means, width/4, label='Coil')
+    rects4 = ax.bar(x + width / 2, total_means, width/4, label='Total')
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Number of residues')
@@ -85,47 +133,8 @@ if __name__ == "__main__":
     ss_composition = SecondaryStructure()
     residue_composition = dict()
 
-    try:
-        # Change the current working Directory
-        print("Directory changed to ", dssp_dir)
-        for dssp_filename in os.listdir(dssp_dir):
-            with open(dssp_dir + dssp_filename, "r") as dssp_file:
-                fasta_filename = dssp_filename.replace("dssp", "fasta")
-                with open(fasta_dir + fasta_filename, "r") as fasta_file:
-                    fasta_line = ''
-                    for line in fasta_file:
-                        if line[0] != '>':
-                            fasta_line = line
-                    for dssp_line in dssp_file:
-                        if dssp_line[0] != '>':
-                            ss_composition.e_count += dssp_line.count("E")
-                            ss_composition.h_count += dssp_line.count("H")
-                            ss_composition.c_count += dssp_line.count("-")
-
-                            if dssp_line.__len__() == fasta_line.__len__():
-                                for i in range(0, dssp_line.__len__()):
-                                    residue = fasta_line[i]
-                                    ss = dssp_line[i]
-                                    if residue in residue_composition.keys():
-                                        obj = residue_composition.get(residue)
-                                    else:
-                                        obj = SecondaryStructure()
-                                    obj.update(ss)
-                                    residue_composition[residue] = obj
-                            else:
-                                print("These files contains different length of string in sequences: ",
-                                      dssp_file)
-    except OSError:
-        print("Can't change the Current Working Directory to ", dssp_dir)
-
-    common_character = ss_composition.e_count + ss_composition.h_count + ss_composition.c_count
-    print("Strand count: ", ss_composition.e_count)
-    print("Helix count: ", ss_composition.h_count)
-    print("Coil count: ", ss_composition.c_count)
-    print("Common character: ", common_character)
-
-    print("Residue composition: ", residue_composition)
-
+    calculate_statistics(ss_composition, residue_composition)
+    print_statistics(ss_composition, residue_composition)
     ss_composition.plot_pie_chart()
     plot_bar_chart(residue_composition)
 
