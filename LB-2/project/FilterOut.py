e6018f7ec1c28eb61d19231f486078f114d15f48
ayakhont <Iohanga731!>
2019-09-12 16:51:33 +0200

    project for LB-2 0.07

--- LB-2/project/FilterOut.py
+++ LB-2/project/FilterOut.py
@@ -5,18 +5,23 @@ def filter_out_blind_data():
     new_list = list()
     with open(blind_data, "r") as f:
         lines = f.readlines()
+        headers = list()
         delete_next_line = False
         for i in range(0, len(lines)):
             line = lines[i]
-            if delete_next_line == True:
-                delete_next_line = False
-                continue
-            if i != (len(lines) -1):
+            next_line = ""
+            if i != (len(lines) - 1):
                 next_line = lines[i+1]
+            if delete_next_line == True:
+                if len(next_line) != 0 and next_line[0] == ">":
+                    delete_next_line = False
+                continue
             if line[0] == ">":
-                if len(next_line) < 50:
+                line = line[0:7] + "\n"
+                if line[0:5] in headers or len(next_line) < 50:
                     delete_next_line = True
                     continue
+                headers.append(line[0:5])
             new_list.append(line)
     with open(blind_data, "w") as file:
         for line in new_list:
