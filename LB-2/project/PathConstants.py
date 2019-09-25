1d9d7a3501876b847d6b6f8524340f6702f838e1
ayakhont <Iohanga731!>
2019-09-25 16:40:11 +0200

    project for LB-2 0.13 (refactoring)

---
+++ LB-2/project/PathConstants.py
@@ -0,0 +1,6 @@
+class PathConstants:
+
+    pdb_dir = "/home/urfin/Education/LB-2/project/data_preparation/pdb_files/"
+    dssp_dir = "/home/urfin/Education/LB-2/project/data_preparation/dssp_files/"
+    dssp_short_dir = "/home/urfin/Education/LB-2/project/data_preparation/dssp_short/"
+    final_blind_data = "/home/urfin/Education/LB-2/project/data_preparation/final_blind_data.txt"
--- LB-2/project/RunDsspCreator.py
+++ LB-2/project/RunDsspCreator.py
@@ -1,19 +1,17 @@
 import os
 import subprocess
+from PathConstants import PathConstants
 from Utils import Utils
 
-pdb_dir = "/home/urfin/Education/LB-2/project/data_preparation/pdb_files/"
-dssp_dir = "/home/urfin/Education/LB-2/project/data_preparation/dssp_files/"
-dssp_short_dir = "/home/urfin/Education/LB-2/project/data_preparation/dssp_short/"
-final_blind_data = "/home/urfin/Education/LB-2/project/data_preparation/final_blind_data.txt"
 
 def prepare_full_dssp_files():
     list_of_pdb_files = list()
-    for pdb_filename in os.listdir(pdb_dir):
+    for pdb_filename in os.listdir(PathConstants.pdb_dir):
         list_of_pdb_files.append(pdb_filename)
 
     for filename in list_of_pdb_files:
-        command = "mkdssp -i " + pdb_dir + filename + " -o " + dssp_dir + filename[0:4] + ".dssp"
+        command = "mkdssp -i " + PathConstants.pdb_dir + filename + " -o " \
+                  + PathConstants.dssp_dir + filename[0:4] + ".dssp"
         print("Running the command: " + command)
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
         process.wait()
@@ -21,11 +19,11 @@ def prepare_full_dssp_files():
 
 # parse each full dssp file with consistency corespondent fasta file
 # and generate for each id short secondary structure dssp file
-def prepare_short_dssp_files():
+def prepare_short_dssp_files(final_blind_data):
     map_id_seq = Utils.get_map_from_fasta_file(final_blind_data)
     map_id_ss = dict()
     for key in map_id_seq.keys():
-        with open(dssp_dir + key[0:4] + ".dssp", "r") as file:
+        with open(PathConstants.dssp_dir + key[0:4] + ".dssp", "r") as file:
             is_start_block = False
             ss = ""
             for line in file:
@@ -37,11 +35,11 @@ def prepare_short_dssp_files():
                     ss += ss_character
             map_id_ss[key] = ss
 
-        with open(dssp_short_dir + key[0:4] + ".dssp", "w") as file:
+        with open(PathConstants.dssp_short_dir + key[0:4] + ".dssp", "w") as file:
             file.write(">" + key + "\n")
             file.write(map_id_ss[key] + "\n")
 
 
 if __name__ == "__main__":
     #prepare_full_dssp_files()
-    prepare_short_dssp_files()
+    prepare_short_dssp_files(PathConstants.final_blind_data)
