0fc4b5d0c4047c1a192eb654cbac3a3e846e9df8
ayakhont <Iohanga731!>
2019-10-10 03:16:08 +0200

    project for LB-2 0.35 (svm profiling)

--- LB-2/project/Svm.py
+++ LB-2/project/Svm.py
@@ -1,8 +1,49 @@
 from sklearn import svm
+from project.PathConstants import PathConstants
 
-x_train = [[0, 0], [0, 1], [1, 0], [1, 1]]
-y_train = [0, 1, 1, 0]
-# Create a SVC with RBF kernel with gamma=0.3 and C=8
-mySVC = svm.SVC(C=8.0, kernel="rbf", gamma=0.3)
-# Train (fit) the model on training data
-mySVC.fit(x_train, y_train)
+
+class Svm:
+
+    window_size: int
+
+    def __init__(self, window_size: int):
+        self.window_size = window_size
+
+    @staticmethod
+    def parse_pssm_by_id(id: str) -> (list, list):
+        dssp_line = ""
+        with open(PathConstants.training_dssp_dir + id + ".dssp", "r") as dssp_file:
+            for dssp_line_in_file in dssp_file:
+                if dssp_line_in_file[0] != '>':
+                    dssp_line = dssp_line_in_file.rstrip()
+        pssm_list = list()
+        with open(PathConstants.profiling_pssm + id + ".pssm", "r") as pssm_file:
+            is_start = False
+            for line in pssm_file:
+                splitted_line = line.split()
+                if splitted_line and splitted_line[0] == "1":
+                    is_start = True
+                if is_start and not splitted_line:
+                    break
+                if is_start:
+                    sub_list = list()
+                    for i in range(22, 42):
+                        sub_list.append(float(splitted_line[i]) / 100)
+                    pssm_list.append(sub_list)
+
+        if len(dssp_line) != len(pssm_list):
+            raise Exception("The lengths of pssm profile and dssp are not equal for: ", id)
+
+        return dssp_line, pssm_list
+
+
+    def train_model(self):
+        x_train = [[0, 0], [0, 1], [1, 0], [1, 1]]
+        y_train = [0, 1, 1, 0]
+        # Create a SVC with RBF kernel with gamma=0.3 and C=8
+        mySVC = svm.SVC(C=8.0, kernel="rbf", gamma=0.3)
+        # Train (fit) the model on training data
+        mySVC.fit(x_train, y_train)
+
+
+y_train, x_train = Svm.parse_pssm_by_id("d1a1xa_")
--- LB-2/project/SvmProfile.py
+++ LB-2/project/SvmProfile.py
@@ -1,6 +1,32 @@
-import numpy as np
+from typing import List
 
+
+# 1 = Helix     : H
+# 2 = Strand    : E
+# 3 = Coil      : C
 class SvmProfile:
 
-    x_train: np.array()
-    y_train: np.array()
+    x_train: List[List[float]]
+    y_train: List[int]
+    window_size: int
+
+    def __init__(self, window_size):
+        self.x_train = list()
+        self.y_train = list()
+        self.window_size = window_size
+
+    def add_element(self, x_vector: List[float], y: int):
+        if len(x_vector) != self.window_size:
+            raise Exception("The size of vector for X in SVM profile doesn't equal to window size!")
+        self.x_train.append(x_vector)
+        self.y_train.append(y)
+
+    @staticmethod
+    def convert_ss_type_to_number(ss_type: str):
+        if ss_type == "H":
+            return 1
+        if ss_type == "E":
+            return 2
+        if ss_type == "-" or ss_type == "C":
+            return 3
+
